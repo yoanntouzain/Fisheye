431ef921fa5d61e0036f9bbb49507d6c69ec2c94
yoanntouzain <ytouzain@gmail.com>
2022-02-02 16:43:37 +0100

    application du filtre dans mon menu déroulant + création du fichier SorterForm.js pour la création de mon menu déroulant

--- Front-End-Fisheye/scripts/pages/DetailsPhotographer.js
+++ Front-End-Fisheye/scripts/pages/DetailsPhotographer.js
@@ -1,75 +1,129 @@
 
 class DetailsPhotographerApps {
     constructor() {
+        this.popularitef = []
         this.$mediaSection = document.querySelector(".media-section")
         this.$photographHeader = document.querySelector(".photograph-header")
+        this.idUrl = document.location.href.substring(document.location.href.lastIndexOf("?id=")+4)
 
+        //Permet de parcourir mon json suivant les données voulues
         this.mediaApi = new MediaApi('/data/photographers.json')
         this.photographersApi = new PhotographersApi("/data/photographers.json")
     }
 
     async HeaderPhotographer() {
-        const idUrl = document.location.href.substring(document.location.href.lastIndexOf("?id=")+4)
         const mediaHeader = await this.photographersApi.getPhotographers()
 
         mediaHeader
             .map(photographers => new Photographers(photographers))
             .forEach(photographers => {
-                if (idUrl == photographers._id) {
+                if (this.idUrl == photographers._id) {
                     const Template = new PhotographerCard(photographers)
                     this.$photographHeader.appendChild(Template.createHeaderPhotographerCard())
                 }
         })
     }
 
-    async SectionPhotographersCard() {
-        const idUrl = document.location.href.substring(document.location.href.lastIndexOf("?id=")+4)
-        const mediaData = await this.mediaApi.getMedia()
+    //on execute le tri par la popularité
+    async SectionPhotographersCardPop() {
         const populariteData = await this.mediaApi.getMedia()
 
-        mediaData
-            .map(media => new Media(media))
-            .forEach(media => {
-                if (idUrl == media._photographerId) {
-                        if (media._video == undefined) {
-                            const Templates = new Picture(media)
-                            this.$mediaSection.appendChild(Templates.createMediaCardPicture())
-                        } else if(media._image == undefined) {
-                            const Templates = new Movie(media)
-                            this.$mediaSection.appendChild(Templates.createMediaCardMovie())
-                        }
-                }
-        })
         bonjour()
         populariteData
             .map(popularite => new Media(popularite))
             .forEach(popularite => {
-                if (idUrl == popularite._photographerId) {
-                    var popularitef = [popularite._likes]
-                    console.log(popularitef)
-                    popularitef.sort(function(a,b) {
-                        return b - a
+                if (this.idUrl == popularite._photographerId) {
+                    this.popularitef.push(popularite)
+                    this.popularitef.sort(function(a,b) {
+                        return b._likes - a._likes
                     })
-                    }
+                }
             })
+        this.popularitef.forEach(popularite => {
+            if (popularite._video == undefined) {
+                const Templates = new Picture(popularite)
+                this.$mediaSection.appendChild(Templates.createMediaCardPicture())
+            } else if(popularite._image == undefined) {
+                const Templates = new Movie(popularite)
+                this.$mediaSection.appendChild(Templates.createMediaCardMovie())
+            }
+        })
+    }
+    
+    //on execute le tri par date
+    async SectionPhotographersCardDate() {
+        const dateData = await this.mediaApi.getMedia()
+
+        dateData
+            .map(date => new Media(date))
+            .forEach(date => {
+                if (this.idUrl == date._photographerId) {
+                    this.popularitef.push(date)
+                    this.popularitef.sort(function compare(a,b) {
+                        if(a._date > b._date) {
+                            return -1
+                        }
+                        if(a._date < b._date) {
+                        return 1 
+                        }
+                        return 0
+                    })
+                    console.log(date)
+                }
+            })
+        this.popularitef.forEach(date => {
+            if (date._video == undefined) {
+                const Templates = new Picture(date)
+                this.$mediaSection.appendChild(Templates.createMediaCardPicture())
+            } else if(date._image == undefined) {
+                const Templates = new Movie(date)
+                this.$mediaSection.appendChild(Templates.createMediaCardMovie())
+            }
+        })
     }
 
+    //on execute le tri par titre
+    async SectionPhotographersCardTitle() {
+        const titleData = await this.mediaApi.getMedia()
 
-    filtre1() {
-    var popularite = [popularite]
-      popularite.sort(function(a,b) {
-        return b - a
-      })
+        
+        titleData
+            .map(title => new Media(title))
+            .forEach(title => {
+                if (this.idUrl == title._photographerId) {
+                    this.popularitef.push(title)
+                    this.popularitef.sort(function compare(a,b) {
+                    if(a._title > b._title) {
+                    return 1
+                    }
+                      if(a._title < b._title) {
+                    return -1
+                    }
+                    if (a._title == undefined) {
+                        return -1
+                    }
+                    return 0
+                  })
+                }
+            })
+        this.popularitef.forEach(title => {
+            if (title._video == undefined) {
+                const Templates = new Picture(title)
+                this.$mediaSection.appendChild(Templates.createMediaCardPicture())
+            } else if(title._image == undefined) {
+                const Templates = new Movie(title)
+                this.$mediaSection.appendChild(Templates.createMediaCardMovie())
+            }
+        })
     }
 
     async FlagLikes() {
-        const idUrl = document.location.href.substring(document.location.href.lastIndexOf("?id=")+4)
         const FlagLikesBar = await this.photographersApi.getPhotographers()
 
         FlagLikesBar
         .map(photographers => new Photographers(photographers))
         .forEach(photographers => {
-            if (idUrl == photographers._id) {
+            if (this.idUrl == photographers._id) {
                 const Template = new PhotographerCard(photographers)
                 main.appendChild(Template.LikePriceDay())
             }
@@ -77,28 +131,45 @@ class DetailsPhotographerApps {
     }
 
 
-    async filterTest() {
-        const filterMedias = await this.mediaApi.getMedia()
-
-        const media = filterMedias.map(media => new Media(media))
-        const Filter = new SorterForm(media)
+    async filterMenu() {
+        const Filter = new SorterForm()
         Filter.render()
     }
 }
-
+// Affiche mon cadre en dessous de mon header
 const Head = new DetailsPhotographerApps()
 Head.HeaderPhotographer()
 
-const Section = new DetailsPhotographerApps()
-Section.SectionPhotographersCard()
+// Affiche mon menu déroulant
+const menu = new DetailsPhotographerApps()
+menu.filterMenu()
 
+// Affiche ma section concernant mes images
+const filtrePop = new DetailsPhotographerApps()
+filtrePop.SectionPhotographersCardPop()
+
+// Affiche mon cadre en bas a droite concernant les likes et le prix/jours
 const Flag = new DetailsPhotographerApps()
 Flag.FlagLikes()
 
-const oui = new DetailsPhotographerApps()
-oui.filterTest()
-
-const non = new DetailsPhotographerApps()
-non.filtre1()
-
 const banniere = new LeCarousel()
+
+// A partir du moment ou je change le filtre dans mon menu déroulant, je vérifie les conditions. Si la valeur de ma séléction de filtre est égal à ma value popularite, 
+// alors je crée une constante qui aura pour valeur une nouvelle instance de ma classe, puis j'executerai ma fonction
+
+var selectElem = document.getElementById("sorter-select")
+
+selectElem.addEventListener('change', function(){
+    var mediaSection = document.querySelector(".media-section")
+    mediaSection.innerHTML = ""
+    if (selectElem.value == "popularite") {
+        const filtrePop = new DetailsPhotographerApps()
+        filtrePop.SectionPhotographersCardPop()
+    } else if (selectElem.value == "date") {
+        const filtreDate = new DetailsPhotographerApps()
+        filtreDate.SectionPhotographersCardDate()
+    } else if (selectElem.value == "title") {
+        const filtreTitle = new DetailsPhotographerApps()
+        filtreTitle.SectionPhotographersCardTitle()
+    }
+})
--- Front-End-Fisheye/scripts/template/SorterForm.js
+++ Front-End-Fisheye/scripts/template/SorterForm.js
@@ -1,23 +1,21 @@
 class SorterForm {
-    constructor(media) {
-        this.media = media
-
+    constructor() {
         this.$wrapper = document.createElement('div')
         this.$sorterFormWrapper = document.querySelector('.sorter-form-wrapper')
     }
+
     render() {
         const filterForm = 
             `<form action="#" method="get" class="sorter-form">
               <label for="sorter-select" class="label-sorter">Triez par: </label>
               <select name="sorter-select" id="sorter-select">
-                <option value="likes">Popularité</option>
+                <option value="popularite" selected>Popularité</option>
                 <option class="test" value="date">Date</option>
                 <option value="title">Titre</option>
               </select>
             </form>`
 
         this.$wrapper.innerHTML = filterForm
-
         this.$sorterFormWrapper.appendChild(this.$wrapper)
     }
 }

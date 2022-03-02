bc12e8e546e52a82c61f84093d308524f5f564cc
yoanntouzain <ytouzain@gmail.com>
2022-03-02 12:47:18 +0100

    clear code

--- Front-End-Fisheye/scripts/utils/contactForm.js
+++ Front-End-Fisheye/scripts/utils/contactForm.js
@@ -1,111 +1,3 @@
-	//Pour la modal du carousel
-function modalsCarousel() {
-	const triggers2 = document.querySelectorAll('.vignette')/*Le bouton qui ouvre la modal */
-
-	const focusableElementsArray2 = [
-	  'button:not([disabled])',
-	  '[tabindex]:not([tabindex="-1"])',
-	  'video:not([disabled])',
-	  'controls:not([disabled])',
-	]
-	const keyCodes2 = {
-	  tab: 9,
-	  enter: 13,
-	  escape: 27,
-	}
-
-	const open2 = function (carousel1) {/*dailog est la div arrière de mon form */
-		
-		const focusableElements = carousel1.querySelectorAll(focusableElementsArray2)/*Correspond a tout les élémentd focusable dans la modale */
-		const firstFocusableElement = focusableElements[0]/*Le bouton X ou la croix qui permet de fermer la modal */
-		const lastFocusableElement = focusableElements[focusableElements.length - 1]/*Permet de séléctionné le bouton valider et que soit soit le dernier focusable*/
-		console.log(firstFocusableElement);
-
-		carousel1.setAttribute('aria-hidden', false)
-
-		if (!firstFocusableElement) {
-			return
-		}
-			
-		window.setTimeout(() => {
-			lastFocusableElement.focus()
-	
-			// trapping focus inside the dialog
-			focusableElements.forEach((focusableElement) => {
-			if (focusableElement.addEventListener) {
-				focusableElement.addEventListener('keydown', (event) => {
-				const tab = event.which === keyCodes2.tab
-	
-				if (!tab) {
-					return
-				}
-	
-				if (event.shiftKey) {
-					if (event.target === firstFocusableElement) { // shift + tab //si tu arrive au premier element avec shift + tab alors retourne le focus a lastFocusableElement
-					event.preventDefault()
-	
-					lastFocusableElement.focus()
-					}
-				} else if (event.target === lastFocusableElement) { // tab //si tu arrive au dernier element avec tab alors retourne le focus a firstFocusableElement
-					event.preventDefault()
-	
-					firstFocusableElement.focus()
-				}
-				})
-			}
-			})
-		}, 100)
-	}
-
-	const close2 = function (carousel1, trigger) {
-		carousel1.setAttribute('aria-hidden', true)
-	
-		// restoring focus
-		trigger.focus()
-	}
-	
-	triggers2.forEach((trigger) => {
-		const carousel1 = document.getElementById(trigger.getAttribute('aria-controls'))/*ajoute l'attribut aria-controls a la div arrière de la modal */
-		const dismissTriggers2 = carousel1.querySelectorAll('[data-dismiss]')/*Le bouton de la croix dans la modal */
-		const focusableElements = carousel1.querySelectorAll(focusableElementsArray2)/*Correspond a tout les élémentd focusable dans la modale */
-		const firstFocusableElement = focusableElements[0]/*Le bouton X ou la croix qui permet de fermer la modal */
-	
-		trigger.addEventListener('click', (event) => {// au click sur le bouton ouvrir ma modal tu execute la fonction open
-			event.preventDefault()
-			open2(carousel1)
-		})
-	
-		trigger.addEventListener('keydown', (event) => {// au press de la touche entrer sur le bouton ouvrir ma modal tu execute la fonction open
-			if (event.which === keyCodes2.enter) {/*event.which correspond au resultat de la touche actionner de la souris, du keycode, ou charcode*/
-			event.preventDefault()
-			open2(carousel1)
-			}
-		})
-
-		carousel1.addEventListener('keyup', (event) => {// au relachement de la touche echap sur le bouton X ma modal tu execute la fonction close
-			if (event.which === keyCodes2.escape) {
-			close2(carousel1, trigger)
-			}
-		})
-		
-		firstFocusableElement.addEventListener('keydown', (event) => {// au relachement de la touche entrer sur le bouton X ma modal tu execute la fonction close
-			if (event.which === keyCodes2.enter) {
-			close2(carousel1, trigger)
-			}
-		})
-
-		dismissTriggers2.forEach((dismissTrigger) => {// c'est le bouton X
-			const dismissCarousel = document.getElementById(dismissTrigger.dataset.dismiss)//on va chercher dans dismissTrigger son dataset et dans dataset on va chercher la valeur de son dismiss qui est dialog
-	
-			dismissTrigger.addEventListener('click', (event) => {
-			event.preventDefault()
-	
-			close2(dismissCarousel, trigger)
-			})
-		})
-	})
-}
-
 	//Pour la modal du formulaire
 function modals() {
 	
@@ -128,7 +20,7 @@ function modals() {
 	
 	
 	///////////////Ouverture de la modal FORM
-	const open = function (dialog) {/*dailog est la div arrière de mon form */
+	function open(dialog) {/*dailog est la div arrière de mon form */
 		const focusableElements = dialog.querySelectorAll(focusableElementsArray)/*Correspond a tout les élémentd focusable dans la modale */
 		const firstFocusableElement = focusableElements[0]/*Le bouton X ou la croix qui permet de fermer la modal */
 		const lastFocusableElement = focusableElements[focusableElements.length - 1]/*Permet de séléctionné le bouton valider et que soit soit le dernier focusable*/
@@ -173,7 +65,7 @@ function modals() {
 
 	///////////////Fin de l'Ouverture de la modal FORM
 	
-	const close = function (dialog, trigger) {
+	function close(dialog, trigger) {
 		dialog.setAttribute('aria-hidden', true)
 		doc.setAttribute('aria-hidden', false)
 	
@@ -215,11 +107,113 @@ function modals() {
 			close(dismissDialog, trigger)
 			})
 		})
+	})
+}
+
+//Pour la modal du carousel
+function modalsCarousel() {
+	const triggers2 = document.querySelectorAll('.vignette')/*Le bouton qui ouvre la modal */
+
+	const focusableElementsArray2 = [
+		'button:not([disabled])',
+		'[tabindex]:not([tabindex="-1"])',
+		'video:not([disabled])',
+		'controls:not([disabled])',
+	]
+	const keyCodes2 = {
+		tab: 9,
+		enter: 13,
+		escape: 27,
+	}
+
+	function open2(carousel1) {/*dailog est la div arrière de mon form */
+		
+		const focusableElements = carousel1.querySelectorAll(focusableElementsArray2)/*Correspond a tout les élémentd focusable dans la modale */
+		const firstFocusableElement = focusableElements[0]/*Le bouton X ou la croix qui permet de fermer la modal */
+		const lastFocusableElement = focusableElements[focusableElements.length - 1]/*Permet de séléctionné le bouton valider et que soit soit le dernier focusable*/
+		console.log(firstFocusableElement);
+
+		carousel1.setAttribute('aria-hidden', false)
+
+		if (!firstFocusableElement) {
+			return
+		}
+			
+		window.setTimeout(() => {
+			lastFocusableElement.focus()
 	
-		window.addEventListener('click', (event) => {// permet de fermer la modal en cliquant sur le la div arrière de modal
-			if (event.target === dialog) {
-			close(dialog, trigger)
+			// trapping focus inside the dialog
+			focusableElements.forEach((focusableElement) => {
+			if (focusableElement.addEventListener) {
+				focusableElement.addEventListener('keydown', (event) => {
+				const tab = event.which === keyCodes2.tab
+	
+				if (!tab) {
+					return
+				}
+	
+				if (event.shiftKey) {
+					if (event.target === firstFocusableElement) { // shift + tab //si tu arrive au premier element avec shift + tab alors retourne le focus a lastFocusableElement
+					event.preventDefault()
+	
+					lastFocusableElement.focus()
+					}
+				} else if (event.target === lastFocusableElement) { // tab //si tu arrive au dernier element avec tab alors retourne le focus a firstFocusableElement
+					event.preventDefault()
+	
+					firstFocusableElement.focus()
+				}
+				})
 			}
+			})
+		}, 100)
+	}
+
+	function close2(carousel1, trigger) {
+		carousel1.setAttribute('aria-hidden', true)
+	
+		// restoring focus
+		trigger.focus()
+	}
+	
+	triggers2.forEach((trigger) => {
+		const carousel1 = document.getElementById(trigger.getAttribute('aria-controls'))/*ajoute l'attribut aria-controls a la div arrière de la modal */
+		const dismissTriggers2 = carousel1.querySelectorAll('[data-dismiss]')/*Le bouton de la croix dans la modal */
+		const focusableElements = carousel1.querySelectorAll(focusableElementsArray2)/*Correspond a tout les élémentd focusable dans la modale */
+		const firstFocusableElement = focusableElements[0]/*Le bouton X ou la croix qui permet de fermer la modal */
+	
+		trigger.addEventListener('click', (event) => {// au click sur le bouton ouvrir ma modal tu execute la fonction open
+			event.preventDefault()
+			open2(carousel1)
+		})
+	
+		trigger.addEventListener('keydown', (event) => {// au press de la touche entrer sur le bouton ouvrir ma modal tu execute la fonction open
+			if (event.which === keyCodes2.enter) {/*event.which correspond au resultat de la touche actionner de la souris, du keycode, ou charcode*/
+			event.preventDefault()
+			open2(carousel1)
+			}
+		})
+
+		carousel1.addEventListener('keyup', (event) => {// au relachement de la touche echap sur le bouton X ma modal tu execute la fonction close
+			if (event.which === keyCodes2.escape) {
+			close2(carousel1, trigger)
+			}
+		})
+		
+		firstFocusableElement.addEventListener('keydown', (event) => {// au relachement de la touche entrer sur le bouton X ma modal tu execute la fonction close
+			if (event.which === keyCodes2.enter) {
+			close2(carousel1, trigger)
+			}
+		})
+
+		dismissTriggers2.forEach((dismissTrigger) => {// c'est le bouton X
+			const dismissCarousel = document.getElementById(dismissTrigger.dataset.dismiss)//on va chercher dans dismissTrigger son dataset et dans dataset on va chercher la valeur de son dismiss qui est dialog
+	
+			dismissTrigger.addEventListener('click', (event) => {
+			event.preventDefault()
+	
+			close2(dismissCarousel, trigger)
+			})
 		})
 	})
 }
